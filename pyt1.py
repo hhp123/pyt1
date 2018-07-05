bbc7b1feb821f4c1a3419a229ad19d53cc90fe34
hhp <1598149049@qq.com>
2018-07-05 15:41:59 +0800

    添加python代码文件：pyt1

---
+++ pyt1.py
@@ -0,0 +1,53 @@
+print("h")
+# 注释
+
+a = 100
+print(a)
+
+a = 'asd'
+print(a)
+print("---------------------------------------")
+# 格式化输出
+print("my name is %s" %a)
+
+age = 21
+name = 'hhp'
+print("my age is %d" %age)
+print("my name is %s, my age is %d" %(name,age))
+
+print("---------------------------------------")
+print("===================")
+name = 'zhangsan'
+QQ = 123456
+tel = 456789
+address = 'jilin'
+print("姓名：%s \nQQ：%s \n手机号：%s \n公司地址：%s \n" %(name, QQ, tel, address))
+
+print("---------------------------------------")
+# 变量赋值 交换变量值
+a = 2
+b = 1
+a, b = 1, 2
+a, b = b, a
+print(a)
+print("---------------------------------------")
+# 讲方法的使用方法调出来
+# help(print())
+# 输入
+# s = input("请输入姓名：")
+# print(s)
+# # 显示类型
+# print(type(s))
+print(8//2.0)
+print(3**2)
+print("---------------------------------------")
+# 类型转换
+x = a
+int(x)
+
+
+
+
+
+
+
